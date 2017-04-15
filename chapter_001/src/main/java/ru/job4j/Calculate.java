25d66ba9c7dcc27c7759f381009dd1972010ba45
Alexey Lepishin <Liberaty1989@gmail.com>
2017-04-15 23:46:02 +0500

    task 4123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,19 +1,18 @@
-/**
-*   Class Класс для вывода в консоль приветственное слово
-*   @Author Alexey Lepishin (lepishin@yandex.ru)
-*   @since 15.04.2017
-*   @version 1
-*/
-
 package ru.job4j;
 
 /**
-*   Метод main
-*   @param args value from arguments
-*/
-
+ * Calculate.
+ *
+ * @author Alexey Leishin (lepishin.ao@yandex.ru)
+ * @version 2
+ * @since 15.04.2017
+ */
 public class Calculate {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
