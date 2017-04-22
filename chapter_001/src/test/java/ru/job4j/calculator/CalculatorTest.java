0b41dfd3d2b93e3eb4fe9059f9660540b540e64c
Alexey Lepishin <Liberaty1989@gmail.com>
2017-04-22 21:57:03 +0500

    tast 185 1

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -26,8 +26,8 @@ public class CalculatorTest {
 	* Test Substruct.
 	*/
     @Test
-    public void whenSubstructTwoMinusOneThenOne {
-        Calculator cal = new Calculator();
+    public void whenSubstructTwoMinusOneThenOne() {
+        Calculator calc = new Calculator();
         calc.substruct(2D, 1D);
         double result = calc.getResult();
         double expected = 1D;
@@ -37,8 +37,8 @@ public class CalculatorTest {
 	* Test Div.
 	*/
     @Test
-    public void whenDivFourDivideTwoThenTwo {
-        Calculator cal = new Calculator();
+    public void whenDivFourDivideTwoThenTwo() {
+        Calculator calc = new Calculator();
         calc.div(4D, 2D);
         double result = calc.getResult();
         double expected = 2D;
@@ -48,8 +48,8 @@ public class CalculatorTest {
 	* Test Multiple.
 	*/
     @Test
-    public void whenMultipleTwoMultiplyTwoThenFour {
-        Calculator cal = new Calculator();
+    public void whenMultipleTwoMultiplyTwoThenFour() {
+        Calculator calc = new Calculator();
         calc.multiple(2D, 2D);
         double result = calc.getResult();
         double expected = 4D;
