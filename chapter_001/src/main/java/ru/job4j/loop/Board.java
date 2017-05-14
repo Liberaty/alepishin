74305228aff03591f8ed35fbbaae9a990d1b0252
Alexey Lepishin <Liberaty1989@gmail.com>
2017-05-14 23:39:11 +0500

    Task 13559

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -9,20 +9,31 @@ package ru.job4j.loop;
 public class Board {
 	/**
 	 * paint.
-	 * @param width - int.
-	 * @param heigth - int.
-	 * @return result - String.
+	 * @param width width.
+	 * @param height height.
+	 * @return result.
 	 */
 	public String paint(int width, int height) {
 		StringBuilder sBuilder = new StringBuilder("");
-		for(int i = 1; i <= height; i++) {
-			for(int j = 1; j <= width; j++) {
-				if(height % 2 == 0) {
-					(width % 2 == 0) ? (sBuilder.append("x")) : (sBuilder.append(" "));
+		for (int i = 1; i <= height; i++) {
+			for (int j = 1; j <= width; j++) {
+				if (i % 2 == 0) {
+					if (j % 2 == 0) {
+						sBuilder.append("x");
+					} else {
+						sBuilder.append(" ");
+					}
+					// j % 2 == 0 ? (sBuilder.append("x")) : (sBuilder.append(" "));
 				} else {
-					(width % 2 == 0) ? (sBuilder.append(" ")) : (sBuilder.append("x"));
+					if (j % 2 == 0) {
+						sBuilder.append(" ");
+					} else {
+						sBuilder.append("x");
+					}
+					// j % 2 == 0 ? (sBuilder.append(" ")) : (sBuilder.append("x"));
 				}
 			}
+			sBuilder.append("\r\n");
 		}
 		String result = sBuilder.toString();
 		return result;
